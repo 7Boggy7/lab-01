2433467be70f736ade1a2260935c3e2659813320
7Boggy7 <bog1dog1mam@gmail.com>
2026-09-22 21:39:00 +0300

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        requestAnimationFrame: 'readonly',
+        cancelAnimationFrame: 'readonly',
+        performance: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      eqeqeq: 'error',
+      'prefer-const': 'warn',
+    },
+  },
+];
